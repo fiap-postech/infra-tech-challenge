920375d3e20fe4b86a4dc827bc2cd82934dc1d47
Eberson Oliveira <eberson.olivera@gmail.com>
2023-11-02 16:27:49 -0300

    createing structure

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  cloud {
+    organization = "fiap-pos-tech"
+
+    workspaces {
+      name = "infra-tech-challenge"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.56.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = "us-east-1"
+  allowed_account_ids = ["212517236385"]
+
+  default_tags {
+    tags = {
+      "worload" = "tech-challenge"
+    }
+  }
+}

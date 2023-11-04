72004be92d41555cfd059499bbdae4987c4fedd2
Eberson Oliveira <eberson.olivera@gmail.com>
2023-11-04 18:24:34 -0300

    fix output cdn address

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "output" {
   value = {
-    cdn_url = "http://${aws_s3_bucket.cdn_bucket.bucket}.s3-website.${local.default_region}.amazonaws.com"
+    cdn_url = "https://${aws_s3_bucket.cdn_bucket.bucket}.s3-website.${local.default_region}.amazonaws.com"
   }
 }

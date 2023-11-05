fc451a2b0488e3214da4275f9b66be23c415f8d9
Eberson Oliveira <eberson.olivera@gmail.com>
2023-11-05 17:07:27 -0300

    added redis provisioning

--- locals.tf
+++ locals.tf
@@ -43,4 +43,37 @@ locals {
       publicly_accessible        = false
     }
   }
+
+  redis = {
+    sg = {
+      name = "cache_sg"
+
+      ingress = {
+        from_port = 6379
+        to_port   = 6379
+        protocol  = "tcp"
+      }
+
+      egress = {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    }
+
+    replication_group = {
+      automatic_failover_enabled = true
+      availability_zones         = ["us-east-1a", "us-east-1b", "sa-east-1c"]
+      replication_group_id       = "tech-challenge-redis"
+      description                = "cache for customer cart purposes"
+      node_type                  = "cache.t3.micro"
+      engine                     = "redis"
+      engine_version             = "6.x"
+      num_cache_clusters         = 1
+      parameter_group_name       = "default.redis6.x"
+      port                       = 6379
+      transit_encryption_enabled = false
+    }
+  }
 }
---
+++ redis.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "redis_sg" {
+  vpc_id = data.aws_vpc.main.id
+  name   = local.redis.sg.name
+
+  ingress {
+    from_port   = local.redis.sg.ingress.from_port
+    to_port     = local.redis.sg.ingress.to_port
+    protocol    = local.redis.sg.ingress.protocol
+    cidr_blocks = [for s in data.aws_subnet.private_selected : s.cidr_block]
+  }
+
+  egress {
+    from_port   = local.redis.sg.egress.from_port
+    to_port     = local.redis.sg.egress.to_port
+    protocol    = local.redis.sg.egress.protocol
+    cidr_blocks = local.redis.sg.egress.cidr_blocks
+  }
+
+  tags = {
+    Name = local.redis.sg.name
+  }
+
+  depends_on = [data.aws_subnet.private_selected]
+}
+
+resource "aws_elasticache_replication_group" "redis" {
+  automatic_failover_enabled = local.redis.replication_group.automatic_failover_enabled
+  availability_zones         = local.redis.replication_group.availability_zones
+  replication_group_id       = local.redis.replication_group.replication_group_id
+  description                = local.redis.replication_group.description
+  node_type                  = local.redis.replication_group.node_type
+  engine                     = local.redis.replication_group.engine
+  engine_version             = local.redis.replication_group.engine_version
+  num_cache_clusters         = local.redis.replication_group.num_cache_clusters
+  parameter_group_name       = local.redis.replication_group.parameter_group_name
+  port                       = local.redis.replication_group.port
+  transit_encryption_enabled = local.redis.replication_group.transit_encryption_enabled
+  security_group_ids         = [aws_security_group.redis_sg.id]
+
+  lifecycle {
+    ignore_changes = [
+      num_cache_clusters
+    ]
+  }
+
+  depends_on = [
+    aws_security_group.redis_sg
+  ]
+}

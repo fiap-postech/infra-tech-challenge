ca2445c9e14be6be0b3a6527fe42e01bef04a16f
Eberson Oliveira <eberson.olivera@gmail.com>
2023-11-05 20:56:37 -0300

    added mysql provider

--- main.tf
+++ main.tf
@@ -14,16 +14,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.56.0"
     }
-  }
-}
 
-provider "aws" {
-  region              = "us-east-1"
-  allowed_account_ids = ["598135944514"]
-
-  default_tags {
-    tags = {
-      "worload" = "tech-challenge"
+    mysql = {
+      source  = "bluemill/mysql"
+      version = "1.11.0"
     }
   }
 }
--- rds.tf
+++ rds.tf
@@ -72,22 +72,10 @@ resource "aws_db_instance" "tech_challenge_db" {
   ]
 }
 
-# resource "null_resource" "db_setup" {
-#   triggers = {
-#     file = filesha1("rds/setup.sql")
-#   }
+resource "mysql_database" "service_database" {
+  name                  = "tech_challenge"
+  default_character_set = "utf8mb4"
+  default_collation     = "utf8mb4_0900_ai_ci"
 
-#   provisioner "local-exec" {
-#     command = <<-EOF
-# 			while read line; do
-# 				echo "$line"
-# 				aws rds-data execute-statement --resource-arn "$DB_ARN" --database  "$DB_NAME" --secret-arn "$SECRET_ARN" --sql "$line"
-# 			done  < <(awk 'BEGIN{RS=";\n"}{gsub(/\n/,""); if(NF>0) {print $0";"}}' initial.sql)
-# 			EOF
-#     environment = {
-#       DB_ENDPOINT = aws_db_instance.tech_challenge_db.endpoint
-#       ADMIN_PASSWORD = data.aws_secretsmanager_secret_version.database_admin_secret_version.secret_string
-#     }
-#     interpreter = ["bash", "-c"]
-#   }
-# }
+  depends_on = [aws_db_instance.tech_challenge_db]
+}
